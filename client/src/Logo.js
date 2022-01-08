905b427ad39f2ed0ef10ae549c59a4f328bec522
Kris Baily <81344693+Kristerran@users.noreply.github.com>
2022-01-08 14:49:19 -0800

    jsx component logo added

---
+++ client/src/Logo.js
@@ -0,0 +1,118 @@
+import React from "react";
+
+export default function SvgComponent() {
+  return (
+    <svg
+      id="el2uOM6W0Em1"
+      viewBox="0 0 80 40"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="el2uOM6W0Em2">
+        <g
+          style={{
+            animation: "el2uOM6W0Em3_to__to 3000ms linear 1 normal forwards",
+          }}
+          transform="translate(40 20)"
+        >
+          <rect
+            height="40"
+            id="el2uOM6W0Em3"
+            width="20"
+            fill="#000"
+            rx="10"
+            ry="10"
+            stroke="none"
+            strokeWidth="1"
+            transform="translate(-10 -20)"
+          />
+        </g>
+        <g
+          style={{
+            animation: "el2uOM6W0Em4_to__to 3000ms linear 1 normal forwards",
+          }}
+          transform="translate(40 20)"
+        >
+          <rect
+            height="40"
+            id="el2uOM6W0Em4"
+            width="20"
+            fill="#000"
+            rx="10"
+            ry="10"
+            stroke="none"
+            strokeWidth="1"
+            transform="translate(-10 -20)"
+          />
+        </g>
+        <g
+          style={{
+            animation: "el2uOM6W0Em5_to__to 3000ms linear 1 normal forwards",
+          }}
+          transform="translate(40 20)"
+        >
+          <rect
+            height="40"
+            id="el2uOM6W0Em5"
+            width="20"
+            fill="#000"
+            rx="10"
+            ry="10"
+            stroke="none"
+            strokeWidth="1"
+            transform="translate(-10 -20)"
+          />
+        </g>
+        <g
+          style={{
+            animation: "el2uOM6W0Em6_to__to 3000ms linear 1 normal forwards",
+          }}
+          transform="translate(40 20)"
+        >
+          <rect
+            height="40"
+            id="el2uOM6W0Em6"
+            width="20"
+            fill="#000"
+            rx="10"
+            ry="10"
+            stroke="none"
+            strokeWidth="1"
+            transform="translate(-10 -20)"
+          />
+        </g>
+      </g>
+      <g
+        style={{
+          animation: "el2uOM6W0Em7_to__to 3000ms linear 1 normal forwards",
+        }}
+        transform="translate(39.803 10.5)"
+      >
+        <g id="el2uOM6W0Em7" transform="translate(-9.803 -10)">
+          <path
+            id="el2uOM6W0Em8"
+            d="M18.606 12.633a9 9 0 11-.096-5.561l-4.255 1.464a4.5 4.5 0 10.048 2.78l4.303 1.317z"
+            fill="#CC99C9"
+            stroke="none"
+            strokeWidth="1"
+          />
+        </g>
+      </g>
+      <g
+        style={{
+          animation: "el2uOM6W0Em9_to__to 3000ms linear 1 normal forwards",
+        }}
+        transform="translate(40 29.5)"
+      >
+        <g id="el2uOM6W0Em9" transform="translate(-10 -29)">
+          <path
+            id="el2uOM6W0Em10"
+            d="M19 29a9 9 0 11-18 0 9 9 0 0118 0zM5.5 29a4.5 4.5 0 109 0 4.5 4.5 0 00-9 0z"
+            fill="#FEB144"
+            stroke="none"
+            strokeWidth="1"
+          />
+        </g>
+      </g>
+    </svg>
+  );
+}
