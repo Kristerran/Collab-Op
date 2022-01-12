ea6a60199f4becc3227670d3a02626ba13e5f844
Kris Baily <81344693+Kristerran@users.noreply.github.com>
2022-01-11 21:39:53 -0800

    Added server scaffold and models

---
+++ server/models/Posting.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const postingSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+});
+
+const Posting = mongoose.model('Posting', postingSchema);
+
+module.exports = Posting;
